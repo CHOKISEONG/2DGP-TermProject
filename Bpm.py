de93337f98305b567285f57a37d81545e123cdaf
Cho Kisung <kisung128@gmail.com>
2025-09-27 23:55:25 +0900

    박자 타이밍 조절

--- Bpm.py
+++ Bpm.py
@@ -4,14 +4,13 @@ class Bpm:
     def __init__(self, bpm):
         self.bpm = bpm
         self.interval = 60.0 / bpm
-        self.coyote_time = 0.1
         self.pvTime = time.time()
 
     def update(self):
         cur_time = time.time()
-        if cur_time - self.pvTime > self.interval:
-            self.pvTime = time.time()
+        diff = cur_time - self.pvTime
+        if diff < self.interval:
+            return diff < self.interval / 10.0 or diff > self.interval * 9.0 / 10.0
+        else:
+            self.pvTime = cur_time
             return True
-        elif cur_time - self.pvTime > self.interval - self.coyote_time:
-            return True
-        return False
