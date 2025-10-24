ee12283d9a6a1703ee283734c6205139802a2e7c
Cho Kisung <kisung128@gmail.com>
2025-10-24 19:55:47 +0900

    구경꾼들이 중심을 보게 + 코드 최적화(gpt 도움)

--- crowd.py
+++ crowd.py
@@ -2,7 +2,7 @@ import random
 from pico2d import *
 
 def get_random_pos():
-    points = [(5, 15), (790, 15), (790, 560), (5, 560)]
+    points = [(5, 20), (790, 20), (790, 595), (5, 595)]
     edges = [
         (points[0], points[1]),
         (points[1], points[2]),
@@ -17,33 +17,26 @@ def get_random_pos():
     return x, y
 
 class Crowd:
-
+    bird_names = ["budgie", "cockatiel", "duck", "parrot", "robin", "sparrow", "toucan"]
     def __init__(self):
-        self.budgie = load_image("birdSheet/Budgie.png")
-        self.cockatiel = load_image("birdSheet/Cockatiel.png")
-        self.duck = load_image("birdSheet/Duck.png")
-        self.parrot = load_image("birdSheet/Parrot.png")
-        self.robin = load_image("birdSheet/Robin.png")
-        self.sparrow = load_image("birdSheet/Sparrow.png")
-        self.toucan = load_image("birdSheet/Toucan.png")
+        self.images = {name: load_image(f"birdSheet/{name.capitalize()}.png") for name in self.bird_names}
+        self.positions = {name: [get_random_pos() for _ in range(15)] for name in self.bird_names}
+        self.directions = {name: [] for name in self.bird_names}
 
-        self.look_left = (0,0,48,48)
-        self.look_right = (48,0,48,48)
+        self.look_left = (0, 0, 48, 48)
+        self.look_right = (48, 0, 48, 48)
 
-        self.budgie_pos = [get_random_pos() for _ in range(20)]
-        self.cockatiel_pos = [get_random_pos() for _ in range(20)]
-        self.duck_pos = [get_random_pos() for _ in range(20)]
-        self.parrot_pos = [get_random_pos() for _ in range(20)]
-        self.robin_pos = [get_random_pos() for _ in range(20)]
-        self.sparrow_pos = [get_random_pos() for _ in range(20)]
-        self.toucan_pos = [get_random_pos() for _ in range(20)]
+        for name in self.bird_names:
+            for pos in self.positions[name]:
+                if pos[0] > 400:
+                    self.directions[name].append(self.look_left)
+                else:
+                    self.directions[name].append(self.look_right)
 
     def draw(self):
-        for i in range(0, 20):
-            self.budgie.clip_draw(*self.look_left, *self.budgie_pos[i])
-            self.cockatiel.clip_draw(*self.look_left, *self.cockatiel_pos[i])
-            self.duck.clip_draw(*self.look_left, *self.duck_pos[i])
-            self.parrot.clip_draw(*self.look_left, *self.parrot_pos[i])
-            self.robin.clip_draw(*self.look_left, *self.robin_pos[i])
-            self.sparrow.clip_draw(*self.look_left, *self.sparrow_pos[i])
-            self.toucan.clip_draw(*self.look_left, *self.toucan_pos[i])
+        for i in range(15):
+            for name in self.bird_names:
+                image = self.images[name]
+                look = self.directions[name][i]
+                pos = self.positions[name][i]
+                image.clip_draw(*look, *pos)
