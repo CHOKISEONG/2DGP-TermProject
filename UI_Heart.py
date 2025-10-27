0b157647c32821a7a0cbfaf2a52d224885c66984
Cho Kisung <kisung128@gmail.com>
2025-10-27 23:14:40 +0900

    박자 도우미 바 이미지

--- UI_Heart.py
+++ UI_Heart.py
@@ -1,16 +1,46 @@
 from pico2d import load_image
+import game_world
+
+class Bar:
+    img = None
+
+    def __init__(self, x):
+        if not Bar.img:
+            Bar.img = load_image('UI/bpm_bar.png')
+        self.area = (0, 0, 64, 64)
+        self.x = x
+        self.dx = 0.1 if self.x == 250 else -0.1
+        self.y = 30
+        self.size = 70
+
+    def update(self, beat_idx):
+        if 399 < self.x < 401:
+            game_world.remove_object(self)
+        else:
+            self.x += self.dx
+
+    def draw(self):
+        self.img.clip_draw(*self.area, self.x, self.y, self.size, self.size)
 
 class UI_Heart():
     def __init__(self):
-        self.img = load_image('UI/heart.png')
-        self.pos = (400,30)
+        self.img_heart = load_image('UI/heart.png')
+        self.area = (0,0,64,64)
+        self.heart_pos = (400,30)
         self.last_beat_idx = -1
         self.size = 50
 
     def update(self, beat_idx):
         if self.last_beat_idx != beat_idx:
             self.size = 60 if self.size == 50 else 50
+            self.generate_bar()
             self.last_beat_idx = beat_idx
 
     def draw(self):
-        self.img.clip_draw(0,0,64,64,*self.pos,self.size,self.size)
+        self.img_heart.clip_draw(*self.area,*self.heart_pos,self.size,self.size)
+
+    def generate_bar(self):
+        bar1 = Bar(250)
+        bar2 = Bar(550)
+        game_world.add_object(bar1)
+        game_world.add_object(bar2)
