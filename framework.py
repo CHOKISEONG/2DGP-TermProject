3d04099a78046bca869b2e4c45eba5c44ec4c4b2
Cho Kisung <kisung128@gmail.com>
2025-10-27 23:15:53 +0900

    60프레임 고정

--- framework.py
+++ framework.py
@@ -4,6 +4,9 @@ from pico2d import *
 running = True
 scene = None
 
+FPS = 60
+frame_time = 1.0 / FPS
+
 def run(main_scene):
     global running, scene
     open_canvas(800,600)
@@ -11,11 +14,17 @@ def run(main_scene):
     scene.enter()
 
     while running:
+        start_time = get_time()
+
         clear_canvas()
         scene.handle_events()
         scene.update()
         scene.draw()
         update_canvas()
+
+        elapsed = get_time() - start_time
+        if elapsed < frame_time:
+            delay(frame_time - elapsed)
 
     scene.exit()
     close_canvas()
