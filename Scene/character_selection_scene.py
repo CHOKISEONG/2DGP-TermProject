80e9600c750b9366237d6985ae4d8e27b637c68b
Cho Kisung <kisung128@gmail.com>
2025-11-07 16:24:20 +0900

    캐릭터 선택창 진입 시 음량 조절 테스트

--- Scene/character_selection_scene.py
+++ Scene/character_selection_scene.py
@@ -1,10 +1,32 @@
 from pico2d import *
 import framework
+import game_world
 from Scene import title_scene
 from Scene import play_scene
 
+character_select_scene = None
+
+class characterSelectScene:
+    def __init__(self):
+        self.start_selected_music = load_wav('Sound/sample/character_select_start.wav')
+        self.start_selected_music.play(1)
+
+        self.title_sound_volume = 100
+
+
+    def update(self, beat_idx):
+        title_scene.title_sound.set_volume(self.title_sound_volume)
+        if self.title_sound_volume > 0:
+            self.title_sound_volume -= 1
+
+    def draw(self):
+        pass
+
 def init():
-    pass
+    print('character_selection_scene init')
+    global character_select_scene
+    character_select_scene = characterSelectScene()
+    game_world.add_object(character_select_scene)
 
 
 def finish():
@@ -16,11 +38,13 @@ def handle_events():
 
 
 def update():
-    pass
+    game_world.update(0)
 
 
 def draw():
-    pass
+    clear_canvas()
+    game_world.render()
+    update_canvas()
 
 
 
--- Scene/title_scene.py
+++ Scene/title_scene.py
@@ -1,43 +1,78 @@
 from pico2d import *
 import framework
+import game_world
 from Scene import play_scene
 from Scene import character_selection_scene
 from Character.crowd import Crowd
 
-image = None
-image_white = None
-start_button = None
-sound = None
-crowd = None
+title = None
+title_sound = None
 mouse_x = 400.0
 mouse_y = 300.0
 
+class TitleImg:
+    def __init__(self):
+        self.title_image = load_image('map/image/title.png')
+
+        self.image_white = load_image('map/image/title_white.png')
+        self.image_white_frame = 0
+        self.df = 1
+
+        self.start_button = load_image('UI/image/start_button.png')
+        self.button_ds = 1.0
+        self.timer = get_time()
+
+        self.crowd = Crowd('title')
+
+        self.my_name = load_font('ENCR10B.TTF', 32)
+
+        self.is_paused = False
+
+    def update(self, beat_idx):
+        cur_time = get_time()
+        if cur_time - self.timer > 0.142:
+            self.timer = cur_time
+            self.image_white_frame += self.df
+            if self.image_white_frame > 16 or self.image_white_frame < 1:
+                self.df = -self.df
+
+    def draw(self):
+        if self.is_paused:
+            self.image_white.clip_draw((self.image_white_frame % 6) * 128, 600 - 96 * (self.image_white_frame // 6 + 1),
+                                       128, 96, 400, 300,
+                                       800, 600)
+            self.title_image.draw(400, 300)
+            self.crowd.draw_title(mouse_x, mouse_y)
+        else:
+            self.image_white.clip_draw((self.image_white_frame % 6) * 128, 600 - 96 * (self.image_white_frame // 6 + 1), 128, 96, 400, 300,
+                                  800, 600)
+            if self.timer >= 6.0:
+                self.title_image.draw(400, 300)
+                self.crowd.draw_title(mouse_x, mouse_y)
+                self.start_button.clip_draw(0, 30, 100, 50, 400, 200, 220 * self.button_ds, 100 * self.button_ds)
+            else:
+                self.my_name.draw(300, 400, '2023182034')
+                self.my_name.draw(300, 350, 'Cho Kiseong')
+
+
+
 def init():
-    global image, image_white, image_white_frame, sound, crowd, img_timer, df, start_button, font, button_ds
-    image = load_image('map/image/title.png')
+    global title, timer, title_sound, my_name
+    title = TitleImg()
+    game_world.add_object(title)
 
-    image_white = load_image('map/image/title_white.png')
-    image_white_frame = 0
-    df = 1
-
-    start_button = load_image('UI/image/start_button.png')
-    button_ds = 1.0
-
-    img_timer = get_time()
-    sound = load_wav('Sound/music/titleMusic.wav')
-    sound.repeat_play()
-    crowd = Crowd('title')
-
-    font = load_font('ENCR10B.TTF', 32)
+    title_sound = load_wav('Sound/music/titleMusic.wav')
+    title_sound.repeat_play()
 
 
 def finish():
-    global image, image_white, image_white_frame, sound, crowd, img_timer, df, start_button, font, button_ds
-    del image, image_white, image_white_frame, sound, crowd, img_timer, df, start_button, font, button_ds
+    print('title_scene finished')
+    global image, image_white, image_white_frame, title_sound, crowd, img_timer, df, start_button, font, button_ds
+    del image, image_white, image_white_frame, title_sound, crowd, img_timer, df, start_button, font, button_ds
 
 
 def handle_events():
-    global mouse_x, mouse_y, button_ds
+    global title, mouse_x, mouse_y, button_ds
 
     event_list = get_events()
     for event in event_list:
@@ -56,39 +91,27 @@ def handle_events():
             elif mouse_x > 500: mouse_x = 500
             if mouse_y < 200: mouse_y = 200
 
-        elif event.type == SDL_MOUSEBUTTONDOWN and img_timer >= 6.0:
+        elif event.type == SDL_MOUSEBUTTONDOWN and title.timer >= 6.0:
             if 310 < mouse_x < 510 and 150 < mouse_y < 230:
                 framework.push_mode(character_selection_scene)
 
 
 def update():
-    global img_timer, image_white_frame, df
-
-    cur_time = get_time()
-    if cur_time - img_timer > 0.142:
-        img_timer = cur_time
-        image_white_frame += df
-        if image_white_frame > 16 or image_white_frame < 1:
-            df = -df
+    game_world.update(0)
 
 
 def draw():
-    global cur_time, mouse_x, mouse_y, button_ds
     clear_canvas()
-
-    image_white.clip_draw((image_white_frame % 6) * 128,600 - 96 * (image_white_frame // 6 + 1),128,96,400,300,800,600)
-    if img_timer >= 6.0:
-        image.draw(400,300)
-        crowd.draw_title(mouse_x, mouse_y)
-        start_button.clip_draw(0,30,100,50,400,200, 220 * button_ds,100 * button_ds)
-    else:
-        font.draw(300,400, '2023182034')
-        font.draw(300,350, 'Cho Kiseong')
-
+    game_world.render()
     update_canvas()
 
 
 
 def pause():
-    print('hi')
-def resume(): pass
+    print('title_scene paused')
+    title.is_paused = True
+    title_sound.set_volume(50)
+
+def resume():
+    print('title_scene resumed')
+    title.is_paused = False
