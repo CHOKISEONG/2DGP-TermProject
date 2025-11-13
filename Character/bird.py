7394db846e262549752ac7dc5e7bc8cb0c5cf2bf
Cho Kisung <kisung128@gmail.com>
2025-11-14 00:12:49 +0900

    shovel_bird 프로토타입 완성

--- Character/bird.py
+++ Character/bird.py
@@ -37,6 +37,7 @@ class AnimationController:
     def draw(self, x, y, w=50, h=50):
         self.img.clip_draw(*self.types[self.current_type][self.frame], x, y, w, h)
 
+
 # 가만히 있을 때
 class Idle:
     def __init__(self, bird):
@@ -70,135 +71,6 @@ class Idle:
     def draw(self):
         self.bird.img.draw(*self.bird.current_pos)
 
-# 타일 놓는 행동을 할 때
-class PlaceTile:
-
-    def __init__(self, bird):
-        self.bird = bird
-        self.start_time = 0
-        self.elapsed_time = 0
-        self.one_beat = 120 / self.bird.bpm
-        self.dy = 0
-
-
-    def enter(self, event):
-        self.start_time = get_time()
-        self.dy = 0
-        key_state = event[1]
-
-        # 누른 방향을 바라보게
-        if SDLK_w in key_state and SDLK_a in key_state:
-            self.bird.img.change_type('up')
-            self.bird.look = DIRECTION.UP_LEFT
-        elif SDLK_w in key_state and SDLK_d in key_state:
-            self.bird.img.change_type('up')
-            self.bird.look = DIRECTION.UP_RIGHT
-        elif SDLK_a in key_state and SDLK_s in key_state:
-            self.bird.img.change_type('down')
-            self.bird.look = DIRECTION.DOWN_LEFT
-        elif SDLK_s in key_state and SDLK_d in key_state:
-            self.bird.img.change_type('down')
-            self.bird.look = DIRECTION.DOWN_RIGHT
-        elif SDLK_a in key_state:
-            self.bird.img.change_type('left')
-            self.bird.look = DIRECTION.LEFT
-        elif SDLK_d in key_state:
-            self.bird.img.change_type('right')
-            self.bird.look = DIRECTION.RIGHT
-
-        # 땅 파기
-        if SDLK_k in key_state:
-            y, x = self.bird.pos_to_row_col(self.bird.pos)
-            nx, ny = 0, 0
-            if self.bird.look == DIRECTION.UP_LEFT:
-                if y % 2 == 1:
-                    ny, nx = y + 1, x
-                else:
-                    ny, nx = y + 1, x - 1
-            elif self.bird.look == DIRECTION.UP_RIGHT:
-                if y % 2 == 1:
-                    ny, nx = y + 1, x + 1
-                else:
-                    ny, nx = y + 1, x
-            elif self.bird.look == DIRECTION.DOWN_LEFT:
-                if y % 2 == 1:
-                    ny, nx = y - 1, x
-                else:
-                    ny, nx = y - 1, x - 1
-            elif self.bird.look == DIRECTION.DOWN_RIGHT:
-                if y % 2 == 1:
-                    ny, nx = y - 1, x + 1
-                else:
-                    ny, nx = y - 1, x
-            elif self.bird.look == DIRECTION.RIGHT:
-                ny, nx = y, x + 1
-            elif self.bird.look == DIRECTION.LEFT:
-                ny, nx = y, x - 1
-            if 0 <= ny < 18 and 0 <= nx < 17:
-                self.bird.field.change_tile(ny, nx, Tile.FALL)
-            return
-
-        # 이동타일 깔기
-        if SDLK_j in key_state:
-            y, x = self.bird.pos_to_row_col(self.bird.pos)
-            nx, ny = 0, 0
-            if self.bird.look == DIRECTION.UP_LEFT:
-                if y % 2 == 1:
-                    ny, nx = y + 1, x
-                else:
-                    ny, nx = y + 1, x - 1
-                if 0 <= ny < 18 and 0 <= nx < 17:
-                    self.bird.field.change_tile(ny, nx, Tile.UP_LEFT)
-            elif self.bird.look == DIRECTION.UP_RIGHT:
-                if y % 2 == 1:
-                    ny, nx = y + 1, x + 1
-                else:
-                    ny, nx = y + 1, x
-                if 0 <= ny < 18 and 0 <= nx < 17:
-                    self.bird.field.change_tile(ny, nx, Tile.UP_RIGHT)
-            elif self.bird.look == DIRECTION.DOWN_LEFT:
-                if y % 2 == 1:
-                    ny, nx = y - 1, x
-                else:
-                    ny, nx = y - 1, x - 1
-                if 0 <= ny < 18 and 0 <= nx < 17:
-                    self.bird.field.change_tile(ny, nx, Tile.DOWN_LEFT)
-            elif self.bird.look == DIRECTION.DOWN_RIGHT:
-                if y % 2 == 1:
-                    ny, nx = y - 1, x + 1
-                else:
-                    ny, nx = y - 1, x
-                if 0 <= ny < 18 and 0 <= nx < 17:
-                    self.bird.field.change_tile(ny, nx, Tile.DOWN_RIGHT)
-            elif self.bird.look == DIRECTION.RIGHT:
-                ny, nx = y, x + 1
-                if 0 <= ny < 18 and 0 <= nx < 17:
-                    self.bird.field.change_tile(ny, nx, Tile.RIGHT)
-            elif self.bird.look == DIRECTION.LEFT:
-                ny, nx = y, x - 1
-                if 0 <= ny < 18 and 0 <= nx < 17:
-                    self.bird.field.change_tile(ny, nx, Tile.LEFT)
-            return
-
-    def exit(self, event):
-        pass
-
-
-    def do(self, beat_idx):
-        self.elapsed_time = get_time() - self.start_time
-        if self.elapsed_time < 0.1:
-            self.dy += 0.03
-        elif self.elapsed_time < 0.2:
-            self.dy -= 0.03
-        else:
-            self.bird.state_machine.handle_state_event(('IDLE', None))
-
-
-    def draw(self):
-        x,y = self.bird.current_pos
-        self.bird.img.draw(x,y + self.dy)
-
-
 # 이동키 처리
 class Move:
     def __init__(self, bird):
@@ -208,25 +80,20 @@ class Move:
     def enter(self, event):
         self.start_time = get_time()
         key_state = event[1]
+        self.bird.look = key_to_direction(key_state)
         # 누른 방향을 바라보게
-        if SDLK_w in key_state and SDLK_a in key_state:
+        if self.bird.look == DIRECTION.UP_LEFT:
             self.bird.img.change_type('up')
-            self.bird.look = DIRECTION.UP_LEFT
-        elif SDLK_w in key_state and SDLK_d in key_state:
+        elif self.bird.look == DIRECTION.UP_RIGHT:
             self.bird.img.change_type('up')
-            self.bird.look = DIRECTION.UP_RIGHT
-        elif SDLK_a in key_state and SDLK_s in key_state:
+        elif self.bird.look == DIRECTION.DOWN_LEFT:
             self.bird.img.change_type('down')
-            self.bird.look = DIRECTION.DOWN_LEFT
-        elif SDLK_s in key_state and SDLK_d in key_state:
+        elif self.bird.look == DIRECTION.DOWN_RIGHT:
             self.bird.img.change_type('down')
-            self.bird.look = DIRECTION.DOWN_RIGHT
-        elif SDLK_a in key_state:
+        elif self.bird.look == DIRECTION.LEFT:
             self.bird.img.change_type('left')
-            self.bird.look = DIRECTION.LEFT
-        elif SDLK_d in key_state:
+        elif self.bird.look == DIRECTION.RIGHT:
             self.bird.img.change_type('right')
-            self.bird.look = DIRECTION.RIGHT
         self.bird.move(self.bird.look)
 
     def exit(self, event):
@@ -236,7 +103,6 @@ class Move:
 
     def draw(self):
         self.bird.img.draw(*self.bird.current_pos)
-
 
 # 특수타일을 밟을 때
 class TileEvent:
@@ -283,7 +149,6 @@ class TileEvent:
 
     def draw(self):
         self.bird.img.draw(*self.bird.current_pos)
-
 
 # 맵에서 떨어질 때
 class Fall:
@@ -338,41 +203,12 @@ class Bird:
 
         self.IDLE = Idle(self)
         self.MOVE = Move(self)
-        self.PLACE_TILE = PlaceTile(self)
         self.TILE_EVENT = TileEvent(self)
         self.FALL = Fall(self)
+
         self.state_machine = StateMachine(
             self.IDLE,
-            {
-                self.IDLE:
-                    {
-                        tile_event: self.TILE_EVENT,
-                        place_tile: self.PLACE_TILE,
-                        move: self.MOVE,
-                        fall: self.FALL
-                    },
-                self.MOVE:
-                    {
-                        move: self.MOVE,
-                        idle: self.IDLE,
-                        tile_event: self.TILE_EVENT
-                    },
-                self.PLACE_TILE:
-                    {
-                        idle: self.IDLE,
-                        place_tile: self.PLACE_TILE
-                    },
-                self.TILE_EVENT:
-                    {
-                        idle: self.IDLE,
-                        fall: self.FALL,
-                        tile_event: self.TILE_EVENT
-                    },
-                self.FALL:
-                    {
-                        idle: self.IDLE
-                    }
-            }
+            {}
         )
 
     def handle_collision(self, group, other):
