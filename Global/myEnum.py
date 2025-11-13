cf7026e66f612b3557777409363fd3614f7d1a06
Cho Kisung <kisung128@gmail.com>
2025-11-13 23:35:14 +0900

    key_state를 인자로 받아서 방향을 리턴하는 함수 만들기

--- Global/myEnum.py
+++ Global/myEnum.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from pico2d import *
 
 # 게임의 6방향을 나타내는 용도 (0은 방향x, 오른쪽 = 1 부터 반시계 방향)
 class DIRECTION(Enum):
@@ -33,6 +34,22 @@ def tile_to_direction(tile):
     elif tile == Tile.LEFT:
         return DIRECTION.LEFT
     elif tile == Tile.RIGHT:
+        return DIRECTION.RIGHT
+    else:
+        return DIRECTION.NONE
+
+def key_to_direction(key_state):
+    if (SDLK_w in key_state and SDLK_a in key_state) or (SDLK_UP in key_state and SDLK_LEFT in key_state):
+        return DIRECTION.UP_LEFT
+    elif (SDLK_w in key_state and SDLK_d in key_state) or (SDLK_RIGHT in key_state and SDLK_UP in key_state):
+        return DIRECTION.UP_RIGHT
+    elif (SDLK_a in key_state and SDLK_s in key_state) or (SDLK_LEFT in key_state and SDLK_DOWN in key_state):
+        return DIRECTION.DOWN_LEFT
+    elif (SDLK_s in key_state and SDLK_d in key_state) or (SDLK_RIGHT in key_state and SDLK_DOWN in key_state):
+        return DIRECTION.DOWN_RIGHT
+    elif SDLK_a in key_state or SDLK_LEFT in key_state:
+        return DIRECTION.LEFT
+    elif SDLK_d in key_state or SDLK_RIGHT in key_state:
         return DIRECTION.RIGHT
     else:
         return DIRECTION.NONE
